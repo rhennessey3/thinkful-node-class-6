f3972aa979b51b35c07565aa5f141e46c5576999
Richard Hennessey III <Rick@Richards-iMac.local>
2020-07-02 20:30:05 -0400

    added

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+
+const server = express()
+
+
+server.get('/urlstring',(req, res) => {
+  
+})
+
+
